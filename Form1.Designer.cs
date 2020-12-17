983fd3b2da8609f0f562fde4571e2168fe7c37c4
Abinavannagendran <abinavan002@gmail.com>
2020-12-17 19:51:50 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace howto_ftp_upload_file
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPickFile = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUri = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnPickFile
+            // 
+            this.btnPickFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPickFile.Image = global::howto_ftp_upload_file.Properties.Resources.Ellipsis;
+            this.btnPickFile.Location = new System.Drawing.Point(465, 108);
+            this.btnPickFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPickFile.Name = "btnPickFile";
+            this.btnPickFile.Size = new System.Drawing.Size(31, 28);
+            this.btnPickFile.TabIndex = 31;
+            this.btnPickFile.UseVisualStyleBackColor = true;
+            this.btnPickFile.Click += new System.EventHandler(this.btnPickFile_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStatus.Location = new System.Drawing.Point(20, 188);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(476, 28);
+            this.lblStatus.TabIndex = 30;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnUpload.Location = new System.Drawing.Point(207, 143);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 28);
+            this.btnUpload.TabIndex = 26;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Location = new System.Drawing.Point(109, 111);
+            this.txtFile.Margin = new System.Windows.Forms.Padding(4);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(347, 22);
+            this.txtFile.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 114);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 17);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "File:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPassword.Location = new System.Drawing.Point(109, 79);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(385, 22);
+            this.txtPassword.TabIndex = 24;
+            this.txtPassword.Text = "123456789";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 82);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsername.Location = new System.Drawing.Point(109, 47);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(385, 22);
+            this.txtUsername.TabIndex = 23;
+            this.txtUsername.Text = "test";
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 50);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Username:";
+            // 
+            // txtUri
+            // 
+            this.txtUri.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUri.Location = new System.Drawing.Point(109, 15);
+            this.txtUri.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUri.Name = "txtUri";
+            this.txtUri.Size = new System.Drawing.Size(385, 22);
+            this.txtUri.TabIndex = 21;
+            this.txtUri.Text = "ftp://192.168.43.55";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "URL:";
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.Title = "Select File";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnUpload;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 233);
+            this.Controls.Add(this.btnPickFile);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUri);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "howto_ftp_upload_file";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPickFile;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUri;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+
+namespace howto_ftp_upload_file
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Start with the executable file.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Done";
+            txtFile.Text = Application.ExecutablePath;
+        }
+
+        // Let the user pick a file.
+        private void btnPickFile_Click(object sender, EventArgs e)
+        {
+            if (ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = ofdFile.FileName;
+            }
+        }
+
+        // Upload the selected file.
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                lblStatus.Text = "Working...";
+                Application.DoEvents();
+
+                FtpUploadFile(txtFile.Text, txtUri.Text,
+                    txtUsername.Text, txtPassword.Text);
+
+                lblStatus.Text = "Done";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Error";
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        // Use FTP to upload a file.
+        private void FtpUploadFile(string filename, string to_uri, string user_name, string password)
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(to_uri);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+
+            // Get network credentials.
+            request.Credentials = new NetworkCredential(user_name.Normalize(), password.Normalize());
+            
+
+            // Read the file's contents into a byte array.
+            byte[] bytes = System.IO.File.ReadAllBytes(filename);
+
+            // Write the bytes into the request stream.
+            request.ContentLength = bytes.Length;
+            using (Stream request_stream = request.GetRequestStream())
+            {
+                request_stream.Write(bytes, 0, bytes.Length);
+                request_stream.Close();
+            }
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
